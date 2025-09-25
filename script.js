4b7c20cbf4a71eed6995dc0fbade622c624a7131
caua <cauaiziel.2008@gmail.com>
2025-09-24 21:20:18 -0300

    criaçao e upload do README.md e dos demais arq.

---
+++ script.js
@@ -0,0 +1,10 @@
+const conselhos = [
+    "acredite em si msm",
+    "o primeiro passo e o mais importante",
+    "a persistencia realiza o impossivel",
+    "aprenda algo novo hoje",
+    "seja gentils com todos ",
+    "jesus te ama"
+];
+
+console.log("rquivo de srcipr carregado. array de conselhos pronto.");
